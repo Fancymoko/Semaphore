fb107e5fdf5d7b21a52a63a4e07c6167f39ec5b6
fancymoko <fancymokojono@gmail.com>
2017-12-04 20:59:56 -0500

    moved everything to one class, tested more thoroughly

--- src/Donut.java
+++ src/Donut.java
@@ -1,18 +1,113 @@
 // @author Steven Turmel
 // @version 02 Dec 2017
 
-public class Donut {
+import java.util.Random;
+import java.util.concurrent.Semaphore;
+
+public class Donut extends Thread{
+    static Table table = new Table();
+    static int count;
+    static Semaphore semHomer = new Semaphore(0);
+    static Semaphore semTable = new Semaphore(1);
+    static Random r;
+    static boolean cont = true;
+
     public static void main(String args[]) {
-        int count;
         if (args.length > 0) {
             count = Integer.parseInt(args[0]);
         } else {
             count = 10;
         }
-        Table table = new Table();
-        new Homer(table, count, "Homer Sprinkles", Item.SPRINKLES);
-        new Homer(table, count, "Homer Filling", Item.FILLING);
-        new Homer(table, count, "Homer Shell", Item.SHELL);
-        new Chef(table, count * 3);
+        Donut donut = new Donut();
+        donut.start();
+    }
+
+    public void run() {
+        Homer homer1 = new Homer("Homer Sprinkles", Item.SPRINKLES);
+        Homer homer2 = new Homer("Homer Filling", Item.FILLING);
+        Homer homer3 = new Homer("Homer Shell", Item.SHELL);
+        Chef chef = new Chef();
+        chef.start();
+        homer1.start();
+        homer2.start();
+        homer3.start();
+        System.out.println("All threads started.");
+    }
+
+    public class Chef extends Thread {
+
+        public Chef() {
+            r = new Random();
+        }
+        public void run() {
+            System.out.println("test");
+            for(int i = 0; i < count; i++) {
+                try {
+                    semTable.acquire();
+                    Item tempItem[] = randomItem();
+                    table.place(tempItem[0], tempItem[1]);
+                    semHomer.release();
+                } catch (InterruptedException e) {
+                    System.err.println(e);
+                }
+            }
+            cont = false;
+        }
+
+        public Item[] randomItem() {
+            int tempInt = r.nextInt(3) + 1;
+            Item[] item;
+            switch (tempInt) {
+                case 1:
+                    item = new Item[] {Item.SPRINKLES, Item.FILLING};
+                    return item;
+                case 2:
+                    item = new Item[] {Item.FILLING, Item.SHELL};
+                    return item;
+                case 3:
+                    item = new Item[] {Item.SHELL, Item.SPRINKLES};
+                    return item;
+                default:
+                    System.out.println("You made an error somewhere in your calculations.");
+                    item = new Item[] {Item.SPRINKLES, Item.FILLING};
+                    return item;
+            }
+        }
+    }
+
+    public class Homer extends Thread{
+        public Item myItem;
+        public Homer(String name, Item item) {
+            setName(name);
+            myItem = item;
+        }
+
+        public void run() {
+            while (cont) {
+                try {
+                    semHomer.acquire();
+                    if (myItem != table.getItem1() && myItem != table.getItem2()) {
+                        table.get(getName());
+                        eatDonut();
+                        semTable.release();
+                    } else {
+                        semHomer.release();
+                        sleep(r.nextInt(5));
+                    }
+                } catch (InterruptedException e) {
+                    System.err.println(e);
+                }
+            }
+        }
+
+        private void eatDonut() {
+            System.out.println( getName() + " is eating the donut.");
+            try {
+                sleep((long) Math.floor(Math.random() * 5));
+            } catch (InterruptedException e) {
+                System.err.println(e);
+            }
+        }
+
     }
 }
--- src/Table.java
+++ src/Table.java
@@ -1,57 +1,48 @@
 // @author Steven Turmel
 // @version 02 Dec 2017
 
-import java.util.concurrent.Semaphore;
 
 public class Table {
-    static Semaphore semChef = new Semaphore(1);
-    static Semaphore semHomer = new Semaphore(0);
+
 
     private Item item1;
     private Item item2;
 
     public void place(Item i, Item j) {
-        try {
-            semChef.acquire();
-        } catch(InterruptedException e) {
-            System.err.println(e);
-        }
         item1 = i;
         item2 = j;
         System.out.println("Chef placed a " + item1.toString() + " and a " + item2.toString());
-        semHomer.release();
     }
 
-    public void get(String name, Item i) {
-        try {
-            semHomer.acquire();
-        } catch(InterruptedException e) {
-            System.err.println(e);
-        }
-        if (i != item1 && i != item2 ) {
-            getItem();
-            System.out.println(name + " ate a donut.");
-            semChef.release();
-        } else
-            semHomer.release();
+    public void get(String name) {
+        getItem();
+        System.out.println(name + " ate a donut.");
     }
 
-    private void placeItem(Item i) {
-        switch (i) {
-            case SPRINKLES:
-                item1 = Item.SPRINKLES;
-                break;
-            case FILLING:
-                break;
-            case SHELL:
-                break;
-            default:
-                System.out.println("You shouldn't get this error");
-        }
-    }
+//    private void placeItem(Item i) {
+//        switch (i) {
+//            case SPRINKLES:
+//                item1 = Item.SPRINKLES;
+//                break;
+//            case FILLING:
+//                break;
+//            case SHELL:
+//                break;
+//            default:
+//                System.out.println("You shouldn't get this error");
+//        }
+//    }
 
     public void getItem() {
         item1 = null;
         item2 = null;
+    }
+
+    public Item getItem1() {
+        return item1;
+    }
+
+    public Item getItem2() {
+        return item2;
     }
 }
